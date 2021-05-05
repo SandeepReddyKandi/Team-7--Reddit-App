4784e6d862a16aa1e40f4f25c6a2c1dbaf6fb9d8
SandeepReddyKandi <SandeepReddyKandi@gmail.com>
2021-05-05 23:33:14 +0530

    added Reddit quick links section

--- frontend/src/views/UserProfile/UserProfile.js
+++ frontend/src/views/UserProfile/UserProfile.js
@@ -114,11 +114,19 @@ class UserProfile extends React.Component {
                                         <a href="https://www.reddithelp.com" className="help-item">help</a>
                                         <a href="https://www.reddit.com/mobile/download" className="help-item">Reddit App</a>
                                         <a href="https://www.reddit.com/coins" className="help-item">Reddit coins</a>
+                                        <a href="https://www.reddit.com/coins" className="help-item">Reddit Premium</a>
+                                        <a href="https://www.reddit.com/coins" className="help-item">Reddit gifts</a>
                                     </div>
                                     <div className="help-col">
-                                        <a href="https://www.reddithelp.com" className="help-item">help</a>
-                                        <a href="https://www.reddit.com/mobile/download" className="help-item">Reddit App</a>
-                                        <a href="https://www.reddit.com/coins" className="help-item">Reddit coins</a>
+                                        <a href="https://www.reddithelp.com" className="help-item">About</a>
+                                        <a href="https://www.reddit.com/mobile/download" className="help-item">Careers</a>
+                                        <a href="https://www.reddit.com/coins" className="help-item">Press</a>
+                                        <a href="https://www.reddit.com/coins" className="help-item">Advertise</a>
+                                        <a href="https://www.reddit.com/coins" className="help-item">Blog</a>
+                                        <a href="https://www.reddit.com/coins" className="help-item">Terms</a>
+                                        <a href="https://www.reddit.com/coins" className="help-item">Content Policy</a>
+                                        <a href="https://www.reddit.com/coins" className="help-item">Privacy Policy</a>
+                                        <a href="https://www.reddit.com/coins" className="help-item">Mod Policy</a>
                                     </div>
                                 </div>
                                 <div className="right-reserved-cont">Team 7 Reddit Clone Inc © 2021.</div>
