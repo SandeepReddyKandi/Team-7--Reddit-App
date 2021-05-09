490677f456d5847f36ccf41e1e2e7266eec55038
Akash Kuratkar <akashkuratkar@Akashs-New-MacBook-Pro.local>
2021-05-08 20:37:56 -0700

    Conflicts resolved

--- backend/src/server/routes/community.js
+++ backend/src/server/routes/community.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { auth, checkAuth } = require( '../utils/passport' )
+const { auth, checkAuth } = require('../utils/passport');
 auth();
 
 const {
@@ -19,27 +19,17 @@ const {
   getInvitations,
 } = require('../controllers/community');
 
-// keep adding end-points here
+router.get('/getInvitations', checkAuth, getInvitations);
 router.post('/invite', checkAuth, sendInvite);
 router.post('/rate', checkAuth, rateCommunity);
 router.post('/add', checkAuth, addCommunity);
 router.get('/communities', checkAuth, getCommunity);
 router.get('/communities', checkAuth, getCommunityById);
 router.post('/status', checkAuth, getStatus);
-router.get('/gettr', checkAuth, getRulesTopics)
+router.get('/gettr', checkAuth, getRulesTopics);
 router.get('/getCommunityByName', checkAuth, getCommunityByName);
-
-router.get('/getInvitations', getInvitations);
-router.post('/invite', sendInvite);
-router.post('/rate', rateCommunity);
-router.post('/add', addCommunity);
-router.get('/communities', getCommunity);
-router.get('/communities', getCommunityById);
-router.post('/status', getStatus);
-router.get('/gettr', getRulesTopics);
-router.get('/getCommunityByName', getCommunityByName);
-router.get('/getCommunityByAdmin', getCommunityByAdmin);
-router.get('/getCommunityByMember', getCommunityByMember);
-router.get('/deletecommunitybyid', deleteCommunityById);
+router.get('/getCommunityByAdmin', checkAuth, getCommunityByAdmin);
+router.get('/getCommunityByMember', checkAuth, getCommunityByMember);
+router.get('/deletecommunitybyid', checkAuth, deleteCommunityById);
 
 module.exports = router;
