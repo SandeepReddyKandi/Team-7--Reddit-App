50e1fd00bda583e9fb8a43e3ca9c5eeb2f934585
SandeepReddyKandi <SandeepReddyKandi@gmail.com>
2021-05-13 14:40:23 +0530

    add comunnity and user info in the response

--- kafka-backend/services/Community/GetCommunityAnalytics.js
+++ kafka-backend/services/Community/GetCommunityAnalytics.js
@@ -1,6 +1,6 @@
 const Community = require("../../models/CommunityModel");
 const Post = require("../../models/PostModel");
-const mongoose = require("mongoose");
+const User = require("../../models/UserModel");
 
 const handle_request = async (req, callback) => {
     try {
@@ -20,36 +20,29 @@ const handle_request = async (req, callback) => {
         if (req.adminId) {
             criteria.admin_id = req.adminId;
         }
-        // Community.find(criteria, (err, response) => {
-        //     if (err) {
-        //         return callback(null, {
-        //             msg: err.message,
-        //             success: false,
-        //         });
-        //     } else {
-        //         console.log('[CB] Response is, ', response);
-        //         return callback(null, {
-        //             msg: "",
-        //             success: true,
-        //             data: response,
-        //         });
-        //     }
-        // });
+
         const agg = Community.aggregate([{$match: {admin_id: req.adminId}}, {
             $project: {
                 membersCount: {$size: '$members'},
                 postsCount: {$size: '$posts'},
                 members: 1,
-                posts: 1
+                posts: 1,
+                images: 1,
+                rules: 1,
+                upvote: 1,
+                downvote: 1,
+                community_id: 1,
+                community_name: 1,
+                description: 1,
+                admin_id: 1,
+                createdAt: 1,
             }
         }])
+
         const postDetailMap = {};
+        const communityDetailQuery = {};
+
         for await (const doc of agg) {
-            console.log('[AA]', doc);
-            // get the count of members
-
-            // get the count of posts
-
             // loop over posts and store the mostUpvotedPost, and create a map for the user who created these posts
             const postsQueryList = doc.posts.filter(post => !!post).map(post => {
                 return new Promise((res) => {
@@ -57,53 +50,88 @@ const handle_request = async (req, callback) => {
                         Post.aggregate(
                             [
                                 {
-                                    $match: {_id: post }
+                                    $match: {_id: post}
                                 },
                                 {
-                                    $project: { upvoteCount: {$size: '$upvote'}, title: 1, author_id: 1, }
+                                    $project: {upvoteCount: {$size: '$upvote'}, title: 1, author_id: 1,}
                                 }
                             ])
                     )
                 });
             })
+            communityDetailQuery[doc._id] = {
+                query: postsQueryList,
+                membersCount: doc.membersCount,
+                postsCount: doc.postsCount,
+                communityId: doc._id
+            };
+        }
 
-            Promise.all(postsQueryList).then(res => {
-                const mostUpVoteObj = { upvoteCount: 0, postId: '', postTitle: ''}
-                const mostActiveUser = { userId: '', postsCounts: 0}
-                const authorPostCountMap = {}
-                res.map(resItem => {
-                    const {upvoteCount,author_id, _id, title } = resItem[0];
-                    if (upvoteCount > mostUpVoteObj.upvoteCount) {
-                        mostUpVoteObj.postId = _id;
-                        mostUpVoteObj.postTitle = title;
-                        mostUpVoteObj.upvoteCount = upvoteCount;
-                    }
-                    authorPostCountMap[author_id] = authorPostCountMap[author_id] ? authorPostCountMap[author_id] + 1 : 1;
-                });
+        for (const comm of Object.keys(communityDetailQuery)) {
+            const { query, ...rest } = communityDetailQuery[comm];
 
-                Object.keys(authorPostCountMap).map(authorId => {
-                    if (isNaN(authorPostCountMap[authorId]) || authorPostCountMap[authorId] > mostActiveUser.postsCounts) {
-                        mostActiveUser.userId = authorId;
-                        mostActiveUser.postsCounts = authorPostCountMap[authorId];
-                    }
-                })
-
-                postDetailMap[doc._id] = {
-                    numOfPosts: doc.postsCount,
-                    membersCount: doc.membersCount,
-                    mostUpVotedPost: mostUpVoteObj,
-                    mostActiveUser: mostActiveUser
-                };
-                console.log('RESULT IN PROMISE ALL IS -- ', postDetailMap)
+            const res = await Promise.all(query);
+            const mostUpVoteObj = { upvoteCount: 0, postId: '', postTitle: ''}
+            const mostActiveUser = { userId: '', postsCounts: 0}
+            const authorPostCountMap = {}
+            res.map(resItem => {
+                const {upvoteCount,author_id, _id, title } = resItem[0];
+                if (upvoteCount > mostUpVoteObj.upvoteCount) {
+                    mostUpVoteObj.postId = _id;
+                    mostUpVoteObj.postTitle = title;
+                    mostUpVoteObj.upvoteCount = upvoteCount;
+                }
+                authorPostCountMap[author_id] = authorPostCountMap[author_id] ? authorPostCountMap[author_id] + 1 : 1;
+            });
+            Object.keys(authorPostCountMap).map(authorId => {
+                if (authorPostCountMap[authorId] > mostActiveUser.postsCounts) {
+                    mostActiveUser.userId = authorId;
+                    mostActiveUser.postsCounts = authorPostCountMap[authorId];
+                }
             })
-            console.log('[LOOP ITERATION ENDS]')
+            // get Community details
+            let communityDetails = {};
+            if (mostUpVoteObj.postId) {
+                communityDetails = await Community.findById( mostUpVoteObj.postId, {
+                    images: 1,
+                    rules: 1,
+                    posts: 1,
+                    members: 1,
+                    upvote: 1,
+                    downvote: 1,
+                    community_id: 1,
+                    community_name: 1,
+                    description: 1,
+                    admin_id: 1,
+                    createdAt: 1,
+                    topic: 1,
+                });
+            }
 
-            // Keep map of postId to { postname, userName } , where userName is who created the most upvotes
-            // Keep map of postId to { postname, mostUpvotedPost, upvoteCount } , where userName is who created the most upvotes
+            // get Community details
+            let userDetails = {};
+            if (mostActiveUser.userId) {
+                userDetails = await User.findById( mostActiveUser.userId, {
+                    userName: 1,
+                    name: 1,
+                    location: 1,
+                    description: 1,
+                    gender: 1,
+                    photo: 1,
+                    topics: 1,
+                    createdAt: 1,
+                });
+            }
+            postDetailMap[comm] = {
+                ...rest,
+                mostUpVotedPost: communityDetails ? { ...communityDetails._doc, ...mostUpVoteObj} : mostUpVoteObj,
+                mostActiveUser: userDetails ? { ...userDetails._doc, ...mostActiveUser} : mostActiveUser,
+            };
+
         }
         return callback(null, {
-            msg: 'error.message',
-            success: false,
+            data: postDetailMap,
+            success: true,
         });
     } catch (error) {
         return callback(null, {
